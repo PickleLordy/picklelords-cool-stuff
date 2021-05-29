bd794f3921954596a2507dcf72440cd706a460f8
PickleLordy <unconfigured@null.spigotmc.org>
2021-05-29 12:37:56 -0600

    Added A The Voidic Plains Biome Tweaked Rarity Of the Void Temple

---
+++ src/main/java/net/mcreator/picklelordscoolstuff/world/biome/VoidicPlainsBiome.java
@@ -0,0 +1,46 @@
+
+package net.mcreator.picklelordscoolstuff.world.biome;
+
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.biome.BiomeGenerationSettings;
+import net.minecraft.world.biome.BiomeAmbience;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.picklelordscoolstuff.PicklelordsCoolStuffModElements;
+
+@PicklelordsCoolStuffModElements.ModElement.Tag
+public class VoidicPlainsBiome extends PicklelordsCoolStuffModElements.ModElement {
+	public static Biome biome;
+	public VoidicPlainsBiome(PicklelordsCoolStuffModElements instance) {
+		super(instance, 5);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new BiomeRegisterHandler());
+	}
+	private static class BiomeRegisterHandler {
+		@SubscribeEvent
+		public void registerBiomes(RegistryEvent.Register<Biome> event) {
+			if (biome == null) {
+				BiomeAmbience effects = new BiomeAmbience.Builder().setFogColor(12638463).setWaterColor(4159204).setWaterFogColor(329011)
+						.withSkyColor(7972607).withFoliageColor(10387789).withGrassColor(9470285).build();
+				BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder().withSurfaceBuilder(
+						SurfaceBuilder.DEFAULT.func_242929_a(new SurfaceBuilderConfig(Blocks.CRACKED_POLISHED_BLACKSTONE_BRICKS.getDefaultState(),
+								Blocks.VOID_AIR.getDefaultState(), Blocks.VOID_AIR.getDefaultState())));
+				MobSpawnInfo.Builder mobSpawnInfo = new MobSpawnInfo.Builder().isValidSpawnBiomeForPlayer();
+				biome = new Biome.Builder().precipitation(Biome.RainType.RAIN).category(Biome.Category.PLAINS).depth(0.1f).scale(0.2f)
+						.temperature(0.5f).downfall(0.5f).setEffects(effects).withMobSpawnSettings(mobSpawnInfo.copy())
+						.withGenerationSettings(biomeGenerationSettings.build()).build();
+				event.getRegistry().register(biome.setRegistryName("picklelords_cool_stuff:voidic_plains"));
+			}
+		}
+	}
+	@Override
+	public void init(FMLCommonSetupEvent event) {
+	}
+}
--- src/main/java/net/mcreator/picklelordscoolstuff/world/structure/VoidTempleStructure.java
+++ src/main/java/net/mcreator/picklelordscoolstuff/world/structure/VoidTempleStructure.java
@@ -57,7 +57,7 @@ public class VoidTempleStructure extends PicklelordsCoolStuffModElements.ModElem
 						dimensionCriteria = true;
 					if (!dimensionCriteria)
 						return false;
-					if ((random.nextInt(1000000) + 1) <= 10000) {
+					if ((random.nextInt(1000000) + 1) <= 3) {
 						int count = random.nextInt(1) + 1;
 						for (int a = 0; a < count; a++) {
 							int i = ci + random.nextInt(16);
